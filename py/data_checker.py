11036c0eb6a1e08d3a50a5af81154fbca4c43065
Cristobal Maldonado <cristobal.maldonado.a@gmail.com>
2022-01-23 03:21:07 -0300

    Fix guid used for packet creation

--- py/data_checker.py
+++ py/data_checker.py
@@ -2,7 +2,6 @@ from calculator import Calculator
 import sys
 import json
 
-
 formula = sys.argv[1]
 rule_meta = json.loads(sys.argv[2])
 filter_map = json.loads(sys.argv[3])
@@ -37,21 +36,6 @@ class Packet:
         return None
 
 
-real_map = {}
-for k in filter_map:
-    packets = filter_map[k]
-    new_packets = set()
-    for packet in packets:
-        p = Packet(packet['guid'])
-        for attribute in packet:
-            if attribute != 'guid':
-                p.add_value(attribute, packet[attribute])
-        new_packets.add(p)
-    real_map[k] = new_packets
-
-solution = Calculator.solve(formula, real_map, rule_meta, vars)
-
-
 class ParserEnconde(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, set):
@@ -62,5 +46,20 @@ class ParserEnconde(json.JSONEncoder):
 
         return super().default(o)
 
+
+real_map = {}
+for k in filter_map:
+    packets = filter_map[k]
+    new_packets = set()
+    for packet in packets:
+        name_id = packet['guid'].split("_")
+        p = Packet(int(name_id[1]))
+        for attribute in packet:
+            if attribute != 'guid':
+                p.add_value(attribute, packet[attribute])
+        new_packets.add(p)
+    real_map[k] = new_packets
+
+solution = Calculator.solve(formula, real_map, rule_meta, vars)
 
 print(json.dumps(solution, cls=ParserEnconde))
