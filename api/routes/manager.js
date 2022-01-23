701a7272367a2f6f5229372a94132e933dc98c6b
Cristobal Maldonado <cristobal.maldonado.a@gmail.com>
2022-01-23 03:18:54 -0300

    Add the registered only middleware

--- api/routes/manager.js
+++ api/routes/manager.js
@@ -1,5 +1,5 @@
 var controller = require("../controllers/manager");
-const { adminOnly } = require("../controllers/authentication");
+const { adminOnly, registeredOnly } = require("../controllers/authentication");
 
 module.exports = function (app) {
   app.put("/rules/:rule", adminOnly, controller.updateRule);
@@ -7,23 +7,23 @@ module.exports = function (app) {
   app.post("/rules", adminOnly, controller.createRule);
   app.post("/rules-multiple/:group", adminOnly, controller.createRuleMultiple);
 
-  app.get("/entities", controller.fetchEntities);
+  app.get("/entities", adminOnly, controller.fetchEntities);
 
-  app.get("/rules/:group/:rule", controller.fetchRule);
-  app.get("/rules/:group", controller.fetchRules);
+  app.get("/rules/:group/:rule", registeredOnly, controller.fetchRule);
+  app.get("/rules/:group", registeredOnly, controller.fetchRules);
 
-  app.get("/groups", controller.fetchGroups);
+  app.get("/groups", registeredOnly, controller.fetchGroups);
   app.post("/groups", adminOnly, controller.createGroup);
-  app.post("/parse_formula", controller.parseFormula);
+  app.post("/parse_formula", adminOnly, controller.parseFormula);
 
-  app.get("/regions", controller.fetchRegions);
-  app.get("/regions/:region", controller.fetchCommunes);
+  app.get("/regions", adminOnly, controller.fetchRegions);
+  app.get("/regions/:region", adminOnly, controller.fetchCommunes);
 
   app.put("/tenders/:tender", adminOnly, controller.updateTender);
   app.post("/tenders", adminOnly, controller.createTender);
   app.delete("/tenders/:tender", adminOnly, controller.deleteTender);
-  app.get("/tenders/:tender", controller.fetchTender);
-  app.get("/tenders", controller.fetchTenders);
+  app.get("/tenders/:tender", registeredOnly, controller.fetchTender);
+  app.get("/tenders", registeredOnly, controller.fetchTenders);
 
   app.get("/self_tenders", adminOnly, controller.fetchUserTenders);
   app.get("/self_rules", adminOnly, controller.fetchUserRules);
