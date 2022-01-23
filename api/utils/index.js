4a76df4df0e07932b8368f5865d26daa2a9f0e12
Cristobal Maldonado <cristobal.maldonado.a@gmail.com>
2022-01-23 03:19:26 -0300

    Update the error handler to display exec errors

--- api/utils/index.js
+++ api/utils/index.js
@@ -1,13 +1,19 @@
 function handleError(message) {
-  var date = new Date();
-  date.toLocaleString();
-  console.error(`[${date}]`, message);
+  const date = new Date();
+  const d = date.toLocaleString();
+
+  var error = message.stderr ? message.stderr : message;
+  error = error instanceof Buffer ? error.toString() : error;
+
+  console.error(`[${d}]`, error);
 }
 
 async function tcWrapper(fn, res) {
   try {
     await fn();
-  } catch (error) {
+  } catch (e) {
+    var error = e.stderr ? e.stderr : e;
+    error = error instanceof Buffer ? error.toString() : error;
     handleError(error);
     res.status(500).json({ status: 500, error });
   }
